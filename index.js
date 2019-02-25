8565799cb536121d382252cdfdbbec400dd46a38
mikelolx <47793088+mikelolx@users.noreply.github.com>
2019-02-25 08:57:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "b!"
+var prefix = "!"
 
 
 
@@ -110,33 +110,6 @@ m.sendMessage(args)
    }
    });
 
-
-   
-
-
-
-
-
-   client.on('message', message => {
-    if (message.content.startsWith("!bot")) {
-    message.channel.send({
-        embed: new Discord.RichEmbed()
-            .setAuthor(client.user.username,client.user.avatarURL)
-            .setThumbnail(client.user.avatarURL)
-            .setColor('RANDOM')
-            .setTitle('``INFO Broadcast Bot`` ')
-            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
-            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
-            .addField('``servers``', [client.guilds.size], true)
-            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
-            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
-            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
-            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
-                  .addField('``My Prefix``' , `[-]` , true)
-                  .addField('``My Language``' , `[ Java Script ]` , true)
-                    })
-}
-});
 
 const adminprefix = "!";//Fras
 const devs = ['ايدي خويك','538063663698018325'];//Fras
